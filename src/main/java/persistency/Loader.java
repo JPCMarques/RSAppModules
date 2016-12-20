25b7b7d9a556ef8adfbb35df0e007270b65328c7
JP Marques <JP Marques>
2016-12-20 15:12:55 +0000

    Updated Loader to use InputStream

--- src/main/java/persistency/Loader.java
+++ src/main/java/persistency/Loader.java
@@ -1,20 +1,23 @@
 package persistency;
 
+import jdk.internal.util.xml.impl.Input;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.InputStream;
 
 /**
  * Created by joaop on 07/12/2016.
  */
 public class Loader {
     protected Unmarshaller unmarshaller;
-    protected String path;
+    protected InputStream inputStream;
     protected Class<?> targetClass;
 
-    public Loader(String path, Class<?> targetClass){
-        this.path = path;
+    public Loader(InputStream inputStream, Class<?> targetClass){
+        this.inputStream = inputStream;
         this.targetClass = targetClass;
     }
 
@@ -22,18 +25,14 @@ public class Loader {
         return JAXBContext.newInstance(targetClass);
     }
 
-    public File generateFile() {
-        return new File(path);
-    }
-
     public void generateUnmarshaller() throws JAXBException {
         unmarshaller = generateContext().createUnmarshaller();
     }
 
     public final Object load() throws JAXBException {
-        File file = generateFile();
         generateUnmarshaller();
-        return unmarshaller.unmarshal(file);
+        return unmarshaller.unmarshal(inputStream);
     }
+
 }
 
